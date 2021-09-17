f1f0da0e17f93cd7a1bb4a7d7e12d8a446a1f11c
Mikulew <mikulew@gmail.com>
2021-09-17 17:36:35 +0200

    Fix overlaping years

--- main.js
+++ main.js
@@ -7,30 +7,32 @@ const minute = 60 * second;
 const hour = 60 * minute;
 const day = 24 * hour;
 
-function getLastDayTime() {
-  const lastDayOfYear = new Date(new Date().getFullYear(), 11, 31, 23,59, 59, 999);
+function getLastDayTime(year) {
+  const lastDayOfYear = new Date(year, 11, 31, 23,59, 59, 999);
   return lastDayOfYear.getTime();
 }
 
 const formatTime = time => time < 10 ? `0${time}` : time;
 
 function countdown() {
+  let currentYear = new Date().getFullYear();
   const todayDate = new Date();
   const todayTime = todayDate.getTime();
-  const endTime = getLastDayTime();
-  const remainingTime = endTime - todayTime;
+  let endTime = getLastDayTime(currentYear);
+  let remainingTime = endTime - todayTime;
   if (endTime < todayDate) {
-    init();
-  } else {
-    const daysLeft = Math.floor(remainingTime / day);
-    const hoursLeft = Math.floor((remainingTime % day) / hour);
-    const minutesLeft = Math.floor((remainingTime % hour) / minute);
-    const secondsLeft = Math.floor((remainingTime % minute) / second);
-    daysBox.textContent = daysLeft;
-    hoursBox.textContent = formatTime(hoursLeft);
-    minutesBox.textContent = formatTime(minutesLeft);
-    secondsBox.textContent = formatTime(secondsLeft);
-  }
+    currentYear += 1;
+    endTime = getLastDayTime(currentYear);
+    remainingTime = endTime - todayTime;
+  } 
+  const daysLeft = Math.floor(remainingTime / day);
+  const hoursLeft = Math.floor((remainingTime % day) / hour);
+  const minutesLeft = Math.floor((remainingTime % hour) / minute);
+  const secondsLeft = Math.floor((remainingTime % minute) / second);
+  daysBox.textContent = daysLeft;
+  hoursBox.textContent = formatTime(hoursLeft);
+  minutesBox.textContent = formatTime(minutesLeft);
+  secondsBox.textContent = formatTime(secondsLeft);
 }
 
 
